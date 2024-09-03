fcd597279a499bd193b762812d5ffa9aa84c08ae
Andressa Lopes <andressacaroline082011@gmail.com>
2024-09-03 14:39:22 -0300

    Certo

--- app/Http/Controllers/RelatorioDiversoController.php
+++ app/Http/Controllers/RelatorioDiversoController.php
@@ -12,72 +12,77 @@ use Illuminate\Support\Facades\Auth;
 class RelatorioDiversoController extends Controller
 {
     public function index(Request $request)
-    {
-        try {
-            $user_id = Auth::id(); // Obtém o ID do usuário autenticado
+{
+    try {
+        $user_id = Auth::id(); // Obtém o ID do usuário autenticado
 
-            // Obtém os parâmetros de filtragem
-            $inicio = $request->query('inicio');
-            $periodo_inicio = $request->query('periodo_inicio');
-            $periodo_fim = $request->query('periodo_fim');
-            $conta = $request->query('conta');
-            $estrategia = $request->query('estrategia');
-            $grupo = $request->query('grupo');
-
-            $query = RelatorioDiverso::where('user_id', $user_id);
-
-            // Aplicar filtros conforme os parâmetros
-            if ($inicio) {
-                $query->whereDate('created_at', '>=', $inicio);
-            }
-
-            if ($periodo_inicio && $periodo_fim) {
-                $query->whereBetween('created_at', [$periodo_inicio, $periodo_fim]);
-            }
-
-            if ($conta) {
-                $query->where('conta', $conta);
-            }
-
-            if ($estrategia) {
-                $query->where('estrategia', $estrategia);
-            }
-
-            if ($grupo) {
-                $query->where('grupo', $grupo);
-            }
-
-            $relatorios = $query->get();
-
-            // Preparar dados para gráficos
-            $rentabilidadeData = [
-                'labels' => $relatorios->pluck('created_at')->map(function ($date) {
-                    return $date->format('d/m/Y');
-                }),
-                'data' => $relatorios->pluck('rentabilidade_periodo')
-            ];
-
-            $rentabilidadeMensalData = [
-                'labels' => $relatorios->groupBy(function ($item) {
-                    return $item->created_at->format('F Y');
-                })->keys(),
-                'data' => $relatorios->groupBy(function ($item) {
-                    return $item->created_at->format('F Y');
-                })->map(function ($items) {
-                    return $items->sum('rentabilidade_periodo');
-                })
-            ];
-
-            return response()->json([
-                'relatorios' => $relatorios,
-                'rentabilidade' => $rentabilidadeData,
-                'rentabilidade_mensal' => $rentabilidadeMensalData,
-            ], Response::HTTP_OK);
-        } catch (\Exception $e) {
-            Log::error('Erro ao listar os relatórios: ' . $e->getMessage());
-            return response()->json(['message' => 'Erro ao listar os relatórios'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        // Verifique se o usuário está autenticado
+        if (!$user_id) {
+            return response()->json(['message' => 'Usuário não autenticado'], Response::HTTP_UNAUTHORIZED);
         }
+
+        // Obtém os parâmetros de filtragem
+        $inicio = $request->query('inicio');
+        $periodo_inicio = $request->query('periodo_inicio');
+        $periodo_fim = $request->query('periodo_fim');
+        $conta = $request->query('conta');
+        $estrategia = $request->query('estrategia');
+        $grupo = $request->query('grupo');
+
+        $query = RelatorioDiverso::where('user_id', $user_id);
+
+        // Aplicar filtros conforme os parâmetros
+        if ($inicio) {
+            $query->whereDate('created_at', '>=', $inicio);
+        }
+
+        if ($periodo_inicio && $periodo_fim) {
+            $query->whereBetween('created_at', [$periodo_inicio, $periodo_fim]);
+        }
+
+        if ($conta) {
+            $query->where('conta', $conta);
+        }
+
+        if ($estrategia) {
+            $query->where('estrategia', $estrategia);
+        }
+
+        if ($grupo) {
+            $query->where('grupo', $grupo);
+        }
+
+        $relatorios = $query->get();
+
+        // Preparar dados para gráficos
+        $rentabilidadeData = [
+            'labels' => $relatorios->pluck('created_at')->map(function ($date) {
+                return $date->format('d/m/Y');
+            }),
+            'data' => $relatorios->pluck('rentabilidade_periodo')
+        ];
+
+        $rentabilidadeMensalData = [
+            'labels' => $relatorios->groupBy(function ($item) {
+                return $item->created_at->format('F Y');
+            })->keys(),
+            'data' => $relatorios->groupBy(function ($item) {
+                return $item->created_at->format('F Y');
+            })->map(function ($items) {
+                return $items->sum('rentabilidade_periodo');
+            })
+        ];
+
+        return response()->json([
+            'relatorios' => $relatorios,
+            'rentabilidade' => $rentabilidadeData,
+            'rentabilidade_mensal' => $rentabilidadeMensalData,
+        ], Response::HTTP_OK);
+    } catch (\Exception $e) {
+        Log::error('Erro ao listar os relatórios: ' . $e->getMessage());
+        return response()->json(['message' => 'Erro ao listar os relatórios'], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
+}
 
 
     public function show($id)
